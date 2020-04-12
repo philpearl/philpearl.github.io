a20c73b4c8c2b12edd8b42c961fee35d1c04f986
Phil Pearl <phil.pearl@ravelin.com>
2020-04-12 13:00:21 +0100

    rebuilding site Sun 12 Apr 2020 13:00:21 BST

---
+++ post/go.mod
@@ -0,0 +1,13 @@
+module github.com/philpearl/blog/content/post
+
+go 1.13
+
+require (
+	github.com/golang/protobuf v1.3.2
+	github.com/json-iterator/go v1.1.9
+	github.com/mailru/easyjson v0.7.1
+	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742
+	github.com/stretchr/testify v1.4.0
+	github.com/tensorflow/tensorflow v2.1.0+incompatible
+	github.com/unravelin/null v2.1.3+incompatible
+)
--- post/jsonbench_test.go
+++ post/jsonbench_test.go
@@ -2,35 +2,113 @@ package badgo
 
 import (
 	"encoding/json"
-	"io/ioutil"
 	"testing"
 	"time"
 
+	jsoniter "github.com/json-iterator/go"
+	easyjson "github.com/mailru/easyjson"
+	"github.com/modern-go/reflect2"
 	"github.com/unravelin/null"
 )
 
 func BenchmarkEncodeMarshaler(b *testing.B) {
-	b.ReportAllocs()
+	jsoniter.RegisterTypeEncoder(reflect2.TypeOf(null.Int{}).String(), nullIntCodec{})
+	jsoniter.RegisterTypeEncoder(reflect2.TypeOf(null.String{}).String(), nullStringCodec{})
+	jsoniter.RegisterTypeEncoder("time.Time", timeCodec{})
 
-	m := struct {
-		A null.Int
-		B time.Time
-		C time.Time
-		D null.String
-	}{
+	m := myteststruct{
 		A: null.IntFrom(42),
 		B: time.Now(),
 		C: time.Now().Add(-time.Hour),
 		D: null.StringFrom(`hello`),
 	}
 
-	b.RunParallel(func(pb *testing.PB) {
-		enc := json.NewEncoder(ioutil.Discard)
-
-		for pb.Next() {
-			if err := enc.Encode(&m); err != nil {
-				b.Fatal("Encode:", err)
+	b.Run("encoding/json", func(b *testing.B) {
+		b.ReportAllocs()
+		b.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				if _, err := json.Marshal(&m); err != nil {
+					b.Fatal("Encode:", err)
+				}
 			}
-		}
+		})
+	})
+
+	b.Run("jsoniter", func(b *testing.B) {
+		b.ReportAllocs()
+		var json = jsoniter.ConfigCompatibleWithStandardLibrary
+		b.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				if _, err := json.Marshal(&m); err != nil {
+					b.Fatal("Encode:", err)
+				}
+			}
+		})
+	})
+
+	b.Run("easyjson", func(b *testing.B) {
+		b.ReportAllocs()
+		b.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				if _, err := easyjson.Marshal(&m); err != nil {
+					b.Fatal("Encode:", err)
+				}
+			}
+		})
+	})
+}
+
+func BenchmarkDecodeMarshaler(b *testing.B) {
+	jsoniter.RegisterTypeDecoder(reflect2.TypeOf(null.Int{}).String(), nullIntCodec{})
+	jsoniter.RegisterTypeDecoder(reflect2.TypeOf(null.String{}).String(), nullStringCodec{})
+	jsoniter.RegisterTypeDecoder("time.Time", timeCodec{})
+
+	m := myteststruct{
+		A: null.IntFrom(42),
+		B: time.Now(),
+		C: time.Now().Add(-time.Hour),
+		D: null.StringFrom(`hello`),
+	}
+
+	data, err := json.Marshal(&m)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.Run("encoding/json", func(b *testing.B) {
+		b.ReportAllocs()
+		b.RunParallel(func(pb *testing.PB) {
+			var n myteststruct
+			for pb.Next() {
+				if err := json.Unmarshal(data, &n); err != nil {
+					b.Fatal(err)
+				}
+			}
+		})
+	})
+
+	b.Run("jsoniter", func(b *testing.B) {
+		b.ReportAllocs()
+		var json = jsoniter.ConfigCompatibleWithStandardLibrary
+		b.RunParallel(func(pb *testing.PB) {
+			var n myteststruct
+			for pb.Next() {
+				if err := json.Unmarshal(data, &n); err != nil {
+					b.Fatal(err)
+				}
+			}
+		})
+	})
+
+	b.Run("easyjson", func(b *testing.B) {
+		b.ReportAllocs()
+		b.RunParallel(func(pb *testing.PB) {
+			var n myteststruct
+			for pb.Next() {
+				if err := easyjson.Unmarshal(data, &n); err != nil {
+					b.Fatal(err)
+				}
+			}
+		})
 	})
 }
